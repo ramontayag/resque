ccd3627b1ef4922d805ddc65911248e58345e1f9
Ramon Tayag <ramon@tayag.net>
2012-05-23 19:45:43 +0800

    Moves resque:work logic into Resque::Launcher

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -6,35 +6,7 @@ namespace :resque do
 
   desc "Start a Resque worker"
   task :work do
-    if ENV['PIDFILE']
-      File.open(ENV['PIDFILE'], 'w') { |f| f << Process.pid }
-    end
-
-    Rake::Task['resque:preload'].invoke
-    Rake::Task['resque:setup'].invoke
-
-    require 'resque'
-
-    queues = (ENV['QUEUES'] || ENV['QUEUE']).to_s.split(',')
-
-    begin
-      worker = Resque::Worker.new(*queues)
-      worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
-      worker.very_verbose = ENV['VVERBOSE']
-    rescue Resque::NoQueueError
-      abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
-    end
-
-    if ENV['BACKGROUND']
-      unless Process.respond_to?('daemon')
-          abort "env var BACKGROUND is set, which requires ruby >= 1.9"
-      end
-      Process.daemon(true)
-    end
-
-    worker.log "Starting worker #{worker}"
-
-    worker.work(ENV['INTERVAL'] || 5) # interval, will block
+    Resque::Launcher.start
   end
 
   desc "Start multiple Resque workers. Should only be used in dev mode."
